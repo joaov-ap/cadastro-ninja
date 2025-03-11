3c72bbca92dca5392530ec797ad490b4590ab3dc
joaov-ap <vitor.1943@gmail.com>
2025-03-11 11:01:29 -0300

    :tada: init: initial commit

---
+++ src/main/java/dev/joaov/cadastroNinja/CadastroDeNinjaApplication.java
@@ -0,0 +1,13 @@
+package dev.joaov.cadastroNinja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/joaov/cadastroNinja/CadastroDeNinjaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joaov.cadastroNinja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
