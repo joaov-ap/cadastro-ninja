a4ab020a83160eef0e3041563ae4d0c8dd27bf10
joaov-ap <vitor.1943@gmail.com>
2025-03-12 11:44:17 -0300

    feat: add Lombok to pom.xml and use annotations to classes NinjaModel and MissoesModel

--- src/main/java/dev/joaov/cadastroNinja/Ninja/NinjaModel.java
+++ src/main/java/dev/joaov/cadastroNinja/Ninja/NinjaModel.java
@@ -2,11 +2,17 @@ package dev.joaov.cadastroNinja.Ninja;
 
 import dev.joaov.cadastroNinja.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_ninja")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -20,45 +26,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, int idade, String email) {
-        this.nome = nome;
-        this.idade = idade;
-        this.email = email;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "NinjaModel{" +
-                "nome='" + nome + '\'' +
-                ", idade=" + idade +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
