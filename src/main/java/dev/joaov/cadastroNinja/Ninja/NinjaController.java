adee91e342066d399c7c343304076c2aadd83910
joaov-ap <vitor.1943@gmail.com>
2025-03-17 10:38:18 -0300

    fix: update ninjas controller route name

--- src/main/java/dev/joaov/cadastroNinja/Ninja/NinjaController.java
+++ src/main/java/dev/joaov/cadastroNinja/Ninja/NinjaController.java
@@ -3,7 +3,7 @@ package dev.joaov.cadastroNinja.Ninja;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("ninjas")
 public class NinjaController {
 
     @PostMapping("/create")
