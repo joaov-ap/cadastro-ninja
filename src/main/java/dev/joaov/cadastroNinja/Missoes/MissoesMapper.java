d3b4bda1627b983b495e833ec81f9d8da06e7d86
joaov-ap <vitor.1943@gmail.com>
2025-03-18 14:16:18 -0300

    chore: add DTO and Mapper to Ninja and Missoes and map manually *Mapper entity x DTO

--- src/main/java/dev/joaov/cadastroNinja/Missoes/MissoesMapper.java
+++ src/main/java/dev/joaov/cadastroNinja/Missoes/MissoesMapper.java
@@ -1,4 +1,28 @@
 package dev.joaov.cadastroNinja.Missoes;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class MissoesMapper {
+    public MissoesModel map(MissoesDTO missoesDTO) {
+        MissoesModel missoes = new MissoesModel();
+
+        missoes.setId(missoesDTO.getId());
+        missoes.setNome(missoesDTO.getNome());
+        missoes.setDificuldade(missoesDTO.getDificuldade());
+        missoes.setNinjas(missoesDTO.getNinjas());
+
+        return missoes;
+    }
+
+    public MissoesDTO map(MissoesModel missoes) {
+        MissoesDTO missoesDTO = new MissoesDTO();
+
+        missoesDTO.setId(missoes.getId());
+        missoesDTO.setNome(missoes.getNome());
+        missoesDTO.setDificuldade(missoes.getDificuldade());
+        missoesDTO.setNinjas(missoes.getNinjas());
+
+        return missoesDTO;
+    }
 }
--- src/main/java/dev/joaov/cadastroNinja/Ninja/NinjaDTO.java
+++ src/main/java/dev/joaov/cadastroNinja/Ninja/NinjaDTO.java
@@ -1,4 +1,20 @@
 package dev.joaov.cadastroNinja.Ninja;
 
+import dev.joaov.cadastroNinja.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String rank;
+    private int idade;
+    private MissoesModel missoesModel;
+
 }
